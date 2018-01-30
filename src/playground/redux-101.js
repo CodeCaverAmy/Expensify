878b100a0748a0c17afa99da87b11c97e21ffbdf
Amy Plant <amy.plant@code-ninja.co>
2018-01-30 16:39:55 -0600

    Use destructuring in our redux-101

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,16 +1,55 @@
 import { createStore } from 'redux';
 
+// Action Generators - functions taht return action objects
+
+// const incrementCount = () => {
+//     return {
+//         type: 'INCREMENT'
+//     };
+// }
+
+// --- same as vvv by wrapping the object {} in parenthesis ({ ... })//
+// const incrementCount = () => ({
+//     type: 'INCREMENT'
+// })
+
+// --- passing payload object in as an argument
+// const incrementCount = (payload = {}) => ({
+//     type: 'INCREMENT',
+//     incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
+// })
+
+// using desctructuring .. the objet passed in (payload) can be replaced by { } with the properties inside
+const incrementCount = ({ incrementBy = 1 } = {}) => ({ // increment default is 1
+    type: 'INCREMENT',
+    //incrementBy: typeof incrementBy === 'number' ? incrementBy : 1
+    // incrementBy: incrementBy
+    incrementBy
+})
+
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+    type: 'DECREMENT',
+    decrementBy
+})
+
+const resetCount = () => ({
+    type: 'RESET',
+})
+
+const setCount = ( { count } = {} ) => ({
+    type: 'SET',
+    count
+})
+
 const store = createStore((state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
-            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
             return {
-                count: state.count + incrementBy
+                count: state.count + action.incrementBy
             };
         case 'DECREMENT':
-            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
             return {
-                count: state.count - decrementBy
+                count: state.count - action.decrementBy
             };
         case 'SET':
             return {
@@ -35,27 +74,24 @@ const unsubscribe = store.subscribe(() => {
 
 // Actions - an object that gets sent to the store - convention: ACTIONS in all CAPS
 // -- increment ---
-store.dispatch({
-    type: 'INCREMENT',
-    incrementBy: 5
-});
-// -- decrement ---
-store.dispatch({
-    type: 'DECREMENT'
-});
+// store.dispatch({
+//     type: 'INCREMENT',
+//     incrementBy: 5
+// });
 
-store.dispatch({
-    type: 'DECREMENT',
-    decrementBy: 10
-});
+store.dispatch(incrementCount({ incrementBy: 5 }))
+
+// // -- decrement ---
+// store.dispatch({
+//     type: 'DECREMENT'
+// });
+
+store.dispatch(decrementCount())
+
+store.dispatch(decrementCount({ decrementBy: 10 }));
 
 // -- reset --
-store.dispatch({
-    type: 'RESET'
-});
+store.dispatch(resetCount());
 
 // -- set --
-store.dispatch({
-    type: 'SET',
-    count: 101
-});
+store.dispatch(setCount({ count: 57 }));
