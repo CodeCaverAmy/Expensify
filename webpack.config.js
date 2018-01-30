1545e3bf7204665486854144b25489e4095a7b0f
Amy Plant <amy.plant@code-ninja.co>
2018-01-29 19:51:43 -0600

    Initial Setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path'); // built in node function
+
+module.exports = {
+    entry: './src/app', // tell webpack where to start
+    output: {
+        path: path.join(__dirname, 'public'), // absolute path on machine to where it lives
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            // loader to tell webpack to run babel everytime it sees a JS file we write
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        },
+        {
+            test: /\.s?css$/, // support scss and css files
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    },
+    // source map for WebPack to help with debugging
+    devtool: 'cheap-module-eval-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public')
+    }
+};
+
