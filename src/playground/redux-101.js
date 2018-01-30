0958a557648ae1ff45330528a444c83a343e5275
Amy Plant <amy.plant@code-ninja.co>
2018-01-30 14:17:35 -0600

    Dispatch generic and dynamic actions using Redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,7 +1,61 @@
 import { createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-    return state;
+const store = createStore((state = { count: 0 }, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+            return {
+                count: state.count + incrementBy
+            };
+        case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1
+            return {
+                count: state.count - decrementBy
+            };
+        case 'SET':
+            return {
+                count: action.count
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        default:
+            return state;
+    }
 });
 
-console.log(store.getState());
+// watch for changes in the store
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
+
+
+// Actions - an object that gets sent to the store - convention: ACTIONS in all CAPS
+// -- increment ---
+store.dispatch({
+    type: 'INCREMENT',
+    incrementBy: 5
+});
+// -- decrement ---
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
+
+// -- reset --
+store.dispatch({
+    type: 'RESET'
+});
+
+// -- set --
+store.dispatch({
+    type: 'SET',
+    count: 101
+});
