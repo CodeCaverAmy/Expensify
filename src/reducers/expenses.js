ca20720e14a96b23e490f77ec9b50228a1fb93d6
Amy Plant <amy.plant@code-ninja.co>
2018-01-31 13:03:39 -0600

    Break out Filters and Expenses reducers into their own files

---
+++ src/reducers/expenses.js
@@ -0,0 +1,30 @@
+// Expenses Reducer
+const expensesReducerDefaultState = [];
+
+export default (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state, // get all of the current values using the spread operator
+                action.expense // add the new expense
+            ];
+    case 'REMOVE_EXPENSE':
+      return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+        // go through every expense in the array to find the match .. when we find the match, correctly change the match
+        return state.map((expense) => { // pass into state.map( ... ) the updater function for the expense
+            if (expense.id === action.id) { 
+                // if the current expense id we are iterating over with map = the expensie id we are trying to edit
+                // return a brand new object (using the object spread operator .. grab all the existing properties)
+                return { // return a new object, changing this particular expense
+                    ...expense, // grab all of the properties on the existing expense
+                    ...action.updates // override any that were passed down
+                }
+            } else {
+                return expense;
+            }
+        })
+    default:
+            return state;
+    }
+};
---
+++ src/reducers/filters.js
@@ -0,0 +1,38 @@
+// Filters Reducer
+const filtersReducersDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+
+export default (state = filtersReducersDefaultState, action) => {
+    switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            // return a new object, getting all of the current values for state (current filter object) 
+            return {
+                ...state,
+                text: action.text // override text, setting it to the text passed in
+            };
+        case 'SET_SORT_BY':
+            // return a new object, getting all of the current values for state (filter object) and set sortBy
+            return {
+                ...state,
+                sortBy: action.sortBy
+            };
+        case 'SET_START_DATE':
+            // return a new object, getting all of the current values for state, and set start Date
+            return {
+                ...state,
+                startDate: action.startDate
+            };
+        case 'SET_END_DATE':
+            // return a new object, getting all of the current values for state, and set start Date
+            return {
+                ...state,
+                endDate: action.endDate
+            };
+        default: 
+            return state;
+    }
+};
