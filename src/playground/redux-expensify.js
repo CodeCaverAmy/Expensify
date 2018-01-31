c307af6e4502e005b402eb83d38eea3c2027eb32
Amy Plant <amy.plant@code-ninja.co>
2018-01-30 20:12:10 -0600

    Create Redux stores that use Multiple Reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,45 @@
-import { createStore, combineReducers } from 'React';
+import { createStore, combineReducers } from 'redux';
+
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SET_START_DATE
+// SET_END_DATE
+
+// Expenses Reducer
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+// Filters Reducer
+const filtersReducersDefaultState = {
+    text: '',
+    sortBuy: 'date',
+    startDate: undefined,
+    endDate: undefined
+};
+const filtersReducer = (state = filtersReducersDefaultState, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
+
+// Store Creation using combineReducers
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+console.log(store.getState());
 
 const demoState = {
     expenses: [{
