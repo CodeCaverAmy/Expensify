c3241326d926af1e52ec14f35e6340339805b26a
Amy Plant <amy.plant@code-ninja.co>
2018-01-31 17:25:28 -0600

    Read from Redux and Dispatch to change the Redux Store

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
 // Export a stateless functional component
 // description, amount, createdAt
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
         <h3>{description}</h3>
         <p>{amount} - {createdAt}</p>
+        <button id={id} onClick={() => {
+            dispatch(removeExpense({ id}));
+            console.log({ id });
+        }}>Remove</button>
     </div>
-)
+);
 
-export default ExpenseListItem;
+// we don't need anything from state, so we don't need mapStateToProps
+export default connect()(ExpenseListItem);
