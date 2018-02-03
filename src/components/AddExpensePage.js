5b09fbcc53a660df559db2b0e792d8471852603e
Amy Plant <amy.plant@code-ninja.co>
2018-02-03 12:11:59 -0600

    Add abiltiy to add and submit expense

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux'; // we wand to connect this component to the store so that we can dispatch
 import ExpenseForm from './ExpenseForm';
+import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = () => (
+// with connect, we now have access to props.dispatch
+const AddExpensePage = (props) => (
     <div>
         <h1>Add Expense</h1>
-        <ExpenseForm />
+        <ExpenseForm 
+            onSubmit={(expense) => {
+                // dispatch the action 'addExpense' passing in the expense
+                props.dispatch(addExpense(expense));
+                // redirect to the dashboard page
+                props.history.push('/');
+            }}
+        />
     </div>
 ); 
 
-export default AddExpensePage;
+export default connect()(AddExpensePage); // we don't need anything from the state, so the first () can be left empty
