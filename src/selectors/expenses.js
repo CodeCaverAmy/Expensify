805669a2e23d11e7db64939ebec7b95377008ef2
Amy Plant <amy.plant@code-ninja.co>
2018-02-03 17:31:08 -0600

    Refactor epenses selector to filter the date range

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,11 +1,13 @@
+import moment from 'moment';
 // Get visible expenses (after sorting)
 // const getVisibleExpenses = (expenses, filters) => { // needs the entire expenses arrays along with the filters
 export default (expenses, { text, sortBy, startDate, endDate }) => { // destructure the filters object
     // use filter to create a new array with all expenses that pass the test implemented by the provided function
     return expenses.filter((expense) => {
+        const createdAtMoment = moment(expense.createdAt);
         // filtering by text, startDate, endDate
-        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDateMatch;
+        const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true;
+        const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         // figure out if expenses.description has the text vriable string inside of it
