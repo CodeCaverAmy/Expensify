0af4217c6ee2278ffaf1b818c54585bd79da826c
Amy Plant <amy.plant@code-ninja.co>
2018-02-03 12:14:35 -0600

    Updates

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux'; // we wand to connect this component to the store so that we can dispatch
 import ExpenseForm from './ExpenseForm';
-import { addExpense } from '../actions/expenses';
 
-// with connect, we now have access to props.dispatch
-const AddExpensePage = (props) => (
+const AddExpensePage = () => (
     <div>
         <h1>Add Expense</h1>
-        <ExpenseForm 
-            onSubmit={(expense) => {
-                // dispatch the action 'addExpense' passing in the expense
-                props.dispatch(addExpense(expense));
-                // redirect to the dashboard page
-                props.history.push('/');
-            }}
-        />
+        <ExpenseForm />
     </div>
 ); 
 
-export default connect()(AddExpensePage); // we don't need anything from the state, so the first () can be left empty
+export default AddExpensePage;
