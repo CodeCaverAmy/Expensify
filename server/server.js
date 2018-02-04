3fc0a77be3b76632f81ecd2f19d46ee4b4c10c9c
Amy Plant <amy.plant@code-ninja.co>
2018-02-04 12:42:28 -0600

    Setup production build and server to push up to Heroku

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const express = require('express'); // require is the 'node' way of importing something
 const app = express(); // create an a new instance of express, we now have an express application
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000; // if the PORT exists in Heroku we use process.env.PORT else use 3000
 
 // sever up the public folder and everything inside
 app.use(express.static(publicPath)); // register middleware
@@ -12,9 +13,9 @@ app.use(express.static(publicPath)); // register middleware
 // called with 2 arguments - 1) path and 2) a function to run w/ request and response object
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
-})
+});
 
 // start up the server on a specific port
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('server is up');
-})
+});
