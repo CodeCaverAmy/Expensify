103f872bca5d6341ad5a4be7688c2e485fc395ff
“CodeCaverAmy” <amyplant@me.com>
2018-02-05 14:00:56 -0600

    Add enzyme-to-json

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,10 +1,14 @@
-import ReactShallowRenderer from 'react-test-renderer/shallow'; // allows us to render our components in regular javascript code and then we can assert something about what got rendered
 import React from 'react';
+import { shallow } from 'enzyme'; // replacing ReactShallowRenderer
 import Header from '../../components/Header';
 
 // test so see if header gets rendered correctly
 test('should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer;
-    renderer.render(<Header />); 
-    expect(renderer.getRenderOutput()).toMatchSnapshot();
+    const wrapper = shallow(<Header />);
+    expect(wrapper).toMatchSnapshot();
+    
+    //expect(wrapper.find('h1').text()).toBe('Expensify');
+    // const renderer = new ReactShallowRenderer;
+    // renderer.render(<Header />); 
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
