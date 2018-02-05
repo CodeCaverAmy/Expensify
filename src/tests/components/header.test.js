f8dbf996c67ff39680586dbcd6850e7f17f0d298
“CodeCaverAmy” <amyplant@me.com>
2018-02-05 13:33:44 -0600

    Setup snap shot testin

--- src/tests/components/header.test.js
+++ src/tests/components/header.test.js
@@ -1,4 +1,10 @@
-// react-test-renderer // allows us to render our components in regular javascript code and then we can assert something about what got rendered
-test('', () => {
+import ReactShallowRenderer from 'react-test-renderer/shallow'; // allows us to render our components in regular javascript code and then we can assert something about what got rendered
+import React from 'react';
+import Header from '../../components/Header';
 
+// test so see if header gets rendered correctly
+test('should render Header correctly', () => {
+    const renderer = new ReactShallowRenderer;
+    renderer.render(<Header />); 
+    expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
