8a14c8cde68e6cd4f28d6bfef6932bca69a2ccbc
“CodeCaverAmy” <amyplant@me.com>
2018-02-05 15:40:49 -0600

    Format currency in the ExpenseListItem

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
-import number from 'numeral';
+import numeral from 'numeral';
 
 // Export a stateless functional component
 // description, amount, createdAt
@@ -10,7 +10,7 @@ const ExpenseListItem = ({ id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
         <p>
-            {amount}: 
+            {numeral(amount / 100).format('$0,0.00')} -  
             {moment(createdAt).format('MMMM do, YYYY')}</p>
     </div>
 );
