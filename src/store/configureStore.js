ab99cf51e1ecdb93c3f5769175ff28b17d6aec35
“CodeCaverAmy” <amyplant@me.com>
2018-02-06 13:54:24 -0600

    Setup configure store to use middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,10 @@
-import { createStore, combineReducers } from 'redux';
-
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
+import thunk from 'redux-thunk';
+
+// if we are using the dev tools || if we are NOT using the devtools ....
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
     const store = createStore(
@@ -9,8 +12,7 @@ export default () => {
             expenses: expensesReducer,
             filters: filtersReducer
         }),
-        // pass in the developer tool as the 2nd argument to take advantge of using the Redux Dev Tools in Chrome
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        composeEnhancers(applyMiddleware(thunk));
     );
 
     return store;
