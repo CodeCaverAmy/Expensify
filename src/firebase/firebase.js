cafe4e2a4f5a2652096081659ecfc3e9307a3878
“CodeCaverAmy” <amyplant@me.com>
2018-02-06 11:14:18 -0600

    Update data in the firebase database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -20,7 +20,11 @@ const database = firebase.database();
 database.ref().set({            // set returns a promise
     name: 'Amy Plant',
     age: 50,
-    isSingle: false,
+    job: {
+        title: 'Software Developer',
+        company: 'AB Data'
+    },
+    stressLevel: 9,
     location: {
         city: 'Milwaukee',
         state: 'Wisconsin', 
@@ -31,6 +35,23 @@ database.ref().set({            // set returns a promise
 }).catch((error) => {
     console.log('oops, looks like something went wrong', error);
 });
+
+// updates - add / update / delete
+database.ref().update({
+    name: 'MJ Brummitt',    // update
+    age: 62,                // update
+    isSingle: null,         // delete
+    job: 'English Teacher', // add
+    // location: {     
+    //     city: 'Mexico City' // only updates at root level, so this will overwite entire location object
+    // }
+    'location/city': 'Mexico City' // this will 'fix' to only modify the city without touching other child objects (location/city are in single quotes due to the / breaking JS)
+});
+
+database.ref().update({
+    'job/company': 'Code Cave Camp', 
+    stressLevel: 3
+})
 
 // removing some part of the database
 
