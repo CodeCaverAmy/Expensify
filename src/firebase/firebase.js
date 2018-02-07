8f6a037309d39911255f5b28137a8141b6ff710d
“CodeCaverAmy” <amyplant@me.com>
2018-02-06 22:22:28 -0600

    Add --dev dotenv and Configure app to accept environment variables for test, dev and prod

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,12 +4,12 @@ import * as firebase from 'firebase'; // * as - takes all the named exports, cre
 
 // set firebase config
 const config = {
-    apiKey: "AIzaSyDdjIS_Wo9tnEpXN-lOu3aZtAhWeMVQsPw",
-    authDomain: "expensify-8f4c7.firebaseapp.com",
-    databaseURL: "https://expensify-8f4c7.firebaseio.com",
-    projectId: "expensify-8f4c7",
-    storageBucket: "expensify-8f4c7.appspot.com",
-    messagingSenderId: "318572610113"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 // make a connection to the database
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,14 @@
 const path = require('path'); // built in node function
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('weback');
+
+process.env.NODE_ENV || 'development';
+
+if (process.env.NODE_ENV === 'test') {
+    require('dotenv').config({ path: '.env.test' });
+} else if (process.env.NODE_ENV === 'development') {
+    require('dotenv').config({ path: '.development' });
+}
 
 module.exports = (env) => {
     // test to see if the build is for production
@@ -41,7 +50,15 @@ module.exports = (env) => {
             }]
         },
         plugins: [
-            CSSExtract
+            CSSExtract,
+            new webpack.DefinePlugin({
+                'pocess.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+                'pocess.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+                'pocess.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+                'pocess.env.FIREBASE_PROJECT': JSON.stringify(process.env.FIREBASE_PROJECT),
+                'pocess.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
+                'pocess.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID)
+            })
         ],
         // source map for WebPack to help with debugging (source-map depends on development or production)
         devtool: isProduction ? 'source-map' : 'inline-source-map',
