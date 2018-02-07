a23b543a9ad0535be1a80201b5d654372f7329c1
“CodeCaverAmy” <amyplant@me.com>
2018-02-06 22:53:19 -0600

    Fix setting of process.env.NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path'); // built in node function
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const webpack = require('weback');
 
-process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
     require('dotenv').config({ path: '.env.test' });
