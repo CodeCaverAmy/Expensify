9b1d307490bae16dd1e3268deb508540a3538881
Amy Plant <amy.plant@code-ninja.co>
2018-02-11 20:32:30 -0600

    Style the filter section, including responsive design styling

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -29,29 +29,38 @@ export class ExpenseListFilters extends React.Component {
     };
     render() {
         return (
-            <div>
-                <input 
-                    type='text' 
-                    value={this.props.filters.text} 
-                    onChange={this.onTextChange}/>
-                <select 
-                    value={this.props.filters.sortBy} 
-                    onChange={this.onSortChange}>
-                    <option value='date'>Date</option>
-                    <option value='amount'>Amount</option>
-                </select>
-                <DateRangePicker
-                    startDate={this.props.filters.startDate}    // required
-                    startDateId="startDateFilter"               // required,
-                    endDate={this.props.filters.endDate}        // required
-                    endDateId="endDateFilter"                   // required,
-                    onDatesChange={this.onDatesChange}          // required
-                    focusedInput={this.state.calendarFocused}   // required
-                    onFocusChange={this.onFocusChange}          // rquired
-                    showClearDates={true}
-                    numberOfMonths={1}
-                    isOutsideRange={() => false}
-                />
+            <div className="content-container">
+                <div className="input-group">
+                    <div className="input-group__item">
+                        <input 
+                            type='text' 
+                            value={this.props.filters.text} 
+                            onChange={this.onTextChange}
+                        />
+                    </div>
+                    <div className="input-group__item">
+                        <select 
+                            value={this.props.filters.sortBy} 
+                            onChange={this.onSortChange}>
+                            <option value='date'>Date</option>
+                            <option value='amount'>Amount</option>
+                        </select>
+                    </div>
+                    <div className="input-group__item">
+                        <DateRangePicker
+                            startDate={this.props.filters.startDate}    // required
+                            startDateId="startDateFilter"               // required,
+                            endDate={this.props.filters.endDate}        // required
+                            endDateId="endDateFilter"                   // required,
+                            onDatesChange={this.onDatesChange}          // required
+                            focusedInput={this.state.calendarFocused}   // required
+                            onFocusChange={this.onFocusChange}          // rquired
+                            showClearDates={true}
+                            numberOfMonths={1}
+                            isOutsideRange={() => false}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
